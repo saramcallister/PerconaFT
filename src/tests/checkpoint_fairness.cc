bd03ec8a7ceb31676c59f369350167c7a30eb25d
Jun <junyuan@cs.stonybrook.edu>
2017-06-06 13:30:50 -0400

    test

--- src/tests/checkpoint_fairness.cc
+++ src/tests/checkpoint_fairness.cc
@@ -117,7 +117,7 @@ int test_main(int argc, char * const argv[]) {
 
     pthread_attr_t tattr;
     pthread_attr_init(&tattr);
-    pthread_attr_setschedpolicy(&tattr, SCHED_RR);
+    pthread_attr_setschedpolicy(&tattr, SCHED_OTHER);
     for (int i=0; i<n_threads; i++) {
 	ids[i]=i;
 	{ int chk_r = toku_pthread_create(&thds[i], &tattr, start_txns, &ids[i]); CKERR(chk_r); }
